f2800057c26d81e2aa7d5f3f9cb8e385eeba93c3
aamree <adilet90@mail.ru>
2020-10-20 20:45:06 +0600

    initial server install socket.io

---
+++ server.ts
@@ -0,0 +1,26 @@
+const express = require('express')
+
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+const PORT = 8000
+const data = new Map()
+
+app.get('/test', function () {
+  console.log('hello');
+  
+})
+
+io.on('connection', socket => {
+  console.log('socket connected', socket);
+  
+})
+
+server.listen(PORT, (err)=>{
+  if (err) {
+    throw Error(err)
+  }
+  console.log("Сервер запущен!");
+  
+})
